3d11a0cd81cf956200dc5c20f1c9eb42df446014
Ana Beatriz Gonçalo de Oliveira <anab.goncalo@gmail.com>
2025-05-21 11:21:03 -0300

    feat: atualizar lógica da classe Main

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -2,9 +2,15 @@ package main;
 
 import controller.UsuarioController;
 import ui.TelaCadastroUsuario;
-import ui.TelaCofreDigital;
 import ui.TelaLogin;
+import ui.TelaCofreDigital;
+import ui.TelaValidacaoToken;
+import ui.TelaValidacaoDuasEtapas;
+import ui.TelaEsqueceuSenha;
+import ui.TelaAdicionarServico;
+import ui.TelaEditarServico;
 
+// IMPORTS RELACIONADOS A CRIAÇÃO DA INTERFACE GRÁFICA
 import javax.swing.*;
 import java.awt.*;
 
@@ -20,12 +26,23 @@ public class Main {
         UsuarioController usuarioController = new UsuarioController();
 
         TelaLogin telaLogin = new TelaLogin(layout, container, usuarioController);
-        TelaCadastroUsuario telaCadastro = new TelaCadastroUsuario(layout, container, usuarioController);
-        TelaCofreDigital telaCofre = new TelaCofreDigital(layout, container, usuarioController);
+        TelaCadastroUsuario telaCadastroUsuario = new TelaCadastroUsuario(layout, container, usuarioController);
+        TelaCofreDigital telaCofreDigital = new TelaCofreDigital(layout, container, usuarioController);
+        TelaValidacaoToken telaValidaToken = new TelaValidacaoToken(layout, container, usuarioController);
+        TelaValidacaoDuasEtapas telaValidacaoDuasEtapas = new TelaValidacaoDuasEtapas(layout, container, usuarioController);
+        TelaEsqueceuSenha telaEsqueceuSenha = new TelaEsqueceuSenha(layout, container, usuarioController);
+        TelaAdicionarServico telaAdicionarServico = new TelaAdicionarServico(layout, container, usuarioController);
+        TelaEditarServico telaEditarServico = new TelaEditarServico(layout, container, usuarioController);
 
         container.add(telaLogin, "telaLogin");
-        container.add(telaCadastro, "telaCadastro");
-        container.add(telaCofre, "telaCofreDigital");
+        container.add(telaCadastroUsuario, "telaCadastroUsuario");
+        container.add(telaCofreDigital, "telaCofreDigital");
+        container.add(telaValidaToken, "telaValidacaoToken");
+        container.add(telaValidacaoDuasEtapas, "telaValidacapDuasEtapas");
+        container.add(telaEsqueceuSenha, "telaEsquceuSenha");
+        container.add(telaAdicionarServico, "telaAdicionarServico");
+        container.add(telaEditarServico, "telaEditarServico");
+
 
         frame.add(container);
         frame.setVisible(true);
