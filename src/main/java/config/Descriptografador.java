970ad2f676eb32451659f0048fc63a7b878ac375
Ana Oliveira <anab.goncalo@gmail.com>
2025-06-03 21:48:48 -0300

    feat: alterando projeto

--- src/main/java/config/Descriptografador.java
+++ src/main/java/config/Descriptografador.java
@@ -3,43 +3,43 @@ package config;
 import javax.crypto.Cipher;
 import javax.crypto.SecretKey;
 import javax.crypto.SecretKeyFactory;
-import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.GCMParameterSpec;
 import javax.crypto.spec.PBEKeySpec;
 import javax.crypto.spec.SecretKeySpec;
-
 import java.security.spec.KeySpec;
 import java.util.Base64;
 
 public class Descriptografador {
-    private Descriptografador() {
-        throw new IllegalStateException("Classe utilitária");
-    }
-
-    private static final String CONTEUDO_CRIPTOGRAFADO = "q2Uk35Ql5FBRBlTPmT1dZQ==:uAbsmm2+BV0bap5aj1pvbA==:zfZSxJ8TDaLv0P/EIlwDb6bADKn9kbi1lOLjQGKSMViTSq3rPFdsss7rfcTmmoiL4iS96d2wVy1eAKXxESlmwM+deB0KbhOgUa/l+ZfUl5Q+0NXjUikjRDytU/u6cHBVz9bYA3jK7dZniQ8dbsHNGO9u4hZD6mZK4FzM582QsVjnnmM79H8T6f2zbZdy5Q7P1J6l98TouRCCiE5IcVwnqSD2c9sHCyXhEVPLwi00Rtc8GuEBWXrYa9ZxD2yqD3s7RxfCexDskpfmU4sPsPgL64f94Zo4SUrnTuDbQT4pAZD3GovHJxcAYdXa0oPMP4b/Nk8o8CJ4tQlWvmsBIa3FBC0OD/zwLA0kfU0OGmnRCAXE4flQf1vxQT3zp4HNCUITrGfYgK6r5ymO7jkiKZrch6ajiaURiMixgju3+rPAGPGSV2Z7P89H4hxvZMlM9AOoppTZXxHVODTeIZFUM0romb4UIhZNXsd1/bjBbzKC3wsPPruysUnLHC0QtbL4YvQxdeyDREXs/pBtlBRbPwicUZWpFsLToDu0r1pP+F/owdkLdgkHblgovKPQYFX6ARxxZ0aTGz159e0LPNoQNoH6aahZs8fruFpI9C+WDcYeIdJw7z4wyHgkzJRY0jR70Zm9ui8RRaVnUc3x0Onn/Z9ZyBIq4VQifuWWjO+QqdnwaDC62gKeWih0KnvuhwfHkK7BQJWYlls9XB3OAgwn61cBQJURFVumuBkbTWmoKYvfHiXnJER2IGAXlDfAFoJS/dW4ECxsreOSmkWPQL+W1HqQ9QgQgNFU/T6dkhzyWhZDgKHm1kayWuoeh0wFvX/OLxk/SD5d2/KI8QDtEZut7ZJZ2Oznz1OgpUklVkCBVgoqik7XAqgdZyK8K54E2Kvtj7toUX7VlJfnHyH57e6AlPOw0glbAnR22IHuqwUZAsbS2TftsECdEYNT1No3SxQc/zTf1TvfcrnWb3ts0BhuJMiD9OQKiWSiY4pAZaltM3XoIgE7oGsqBbZSQ5M2u+H/gMILRNpntOQWTEKiuRQvA49xaOn596vXtNx7vIxLbqrONoCL5FQIhlgvDvwSO+cOYZfLfVLi9RAwXbiEW5+wlJeAWwdzTC0LtORWVFlb7GBpi5d8lHeMro6MoBOM3kjxN/es1trd1op1NzmdNOV3ozi3d4khK6NcTBDXywFip9xlm0jCnlA62aSo84SALIeBZEwAf36afbwR6TZYWfGlHHUYSmGuN/rI1flU87mZuyCh+6t+ObL+BpTaqal+R5Yw2l6GoHFWOY4hEfQZAM+3kUkxEClWjakzX09hD5Wk3H7d3f01VrOYDv1j+iKtWGKnemxtXLKDElC5NA9HOULjsahpyouJ1jpFoW7tUxTCOcS1gsHNgazv5KzFX5BbYFCwaTRLT759CAvEqx9lCj27AkFE4Dz+BoDF3/DYr/b9wsiFkV1iLxyPjvc/Nv4nUFL80xGTbCJwiujfC7IoHG10m0kJqYrrNryVJc2aUrdVpQqBglKhvr3IuF37JIOkboQchdeBc5IPpS5E9eko47CTlWTvwtR9i43evKdFsJIy4XNQ/pN1EEXLIc6pXJnRVoGxuT6pjEAaoP49UFlFBEKZQztNwXJB4NZ5/Ic9Wp06oeGCTwhqoSp0i6WnRFVRf3URC55gi6GcXRI1ph8hYbjAQoCpLyrnWnLzrGhNfR4MQa0i+Bjvl7B0D3bSUsgXkqUR3SeRnBkqHzH2xDjHejUAbDU5yc/H0CYVDqJCxDv6JNJehhgnwRaUwcsHeZ+mBKErp271W8ZblRjoE6H91fKc+cNmn6Df/RfNTymH76liMCNK4BGAZyO0CtjmilTpFjT/Q+cEJoXk2dhaN2Rt02LUG0FW1D1TdywgcW/b/E67G6JhvQnpjHyTZWO2VpFsNjt/lZOd7Bxu4VHTYV3h6bpIWPh6pHtcUfxvKErs2iQDHqDXhdvh/QOR5Mc1VvQ5sPrlzgEF/SSjjU3saZl2wtBfgcVIWD1dtz1BpzEJkW8PELZzbVlwQ6yOQIWTQej4dPNAx7/nf1dRNq1HhaBw+ymT3mqRIXEtnvEc9f4kfBR3a0DNFXcuAZ/2wH5e7v4iMQbGnwiu3MficHfNiZBL6GKPJb0Rg9CvBQe3UdtBUfIJTxHwkly8ZkBOtOgQSARzh1uu5IdMHHr87n+0ntweqY7E3I6Ilh9cJr+zDllgodb+Od2PK+QelEgwx3c8I6hkLTJWuwTxIYi2u60870Lv2WfmbkWDeg7MaghBqyPGvVeLckRpQ6KyAC5t2kHKHODzgcoZn4O/qkN8eX2Oi1sEGiTjuS/W9JBx31SSuUIisXogqlFpyzr/7FXRYHggmC2JzV1IeWIkfriuR0VnHGU9V5eZ5AmxmkivKn0rqwqXIv9qLvV9WxLQ2Nlq0+h1f9nVcJKkRudjtyei6a3MBTTNv88tOU+lrars3G1CaQ+m03N0giXC2V3rqgTdZ39odzW0OcrLg1Ae+aPFX3uGBNKBQJBDbh/5LeiWB6MhUP+mwjySJa81kYKtITQojRt+QsbvTtFt20AwLx7RE8cNjM+ch+Wi/Mmo4A4TJTh+kIDnPdxBdEMFdyUc6Xet3mXMcmtqObVxaWxZQY1PiRWuiXJJLXMYZmmjh/XNobunl7maTyjyyO8YeXu7LMj/uvExoz71z+sWU7jeKKS7qwbKHUHECSfui6dzMyXGPLj3nYk6STZhIFjoEQZafoT48uyAPLPMir3RE4XBnMb+JHfs17EL0Ykf4tG32HeAmemRHYALjZiyrKwUbsWaegU6+lxJYKQEOgPZPhmax99r2eDPz5eCTXcROztjdcZ3EHrZnRojfu5pOjkONpud/4Bq++IYxq00jhPtC6RHRdGhlbsdq+N70M3tmWQZbgv7txy//1lbq1UgjEGZXR4gViI9ER0H/E132CLhQ792kUrbvLC5S8w8HTyu0wozvIFVHt2tNucCPL1/dnd/U3BCgX/7DT5UtH2rwrbouDPOG3d+gA3H50Qe2nTSOcEwy3GafGKGCiwQwSIs64CRmSActMcmMJAa0vQd7rcKrDJ8yg0YnLfga1XTrTnsZiQOU1mvRXBgxzQokP0Jj6j7milPWEkUvfRvxSlP6XV+kaeWXmPJXjyLuu37+dC6fQCYHxrCtAwpHi/eMQLCNyQ3P4Y=";
-
-    private static SecretKey gerarChaveSecreta(char[] senha, byte[] salt) throws Exception {
-        SecretKeyFactory fabrica = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
-        KeySpec espec = new PBEKeySpec(senha, salt, 65536, 256);
-        SecretKey chaveTemp = fabrica.generateSecret(espec);
-        return new SecretKeySpec(chaveTemp.getEncoded(), "AES");
-    }
+    private static final int TAG_LENGTH_BIT = 128;
 
     public static String descriptografarArquivo(String senha) throws Exception {
-        String[] partes = CONTEUDO_CRIPTOGRAFADO.split(":");
+        
+        String conteudoCriptografado = "H4RMl9Sghnco2HeJWT5hNQ==:vRfko4LWPzT3qFsw:G+36vsWg4/N6OkFGk0WZzncfN+JP0zjAtXSFhORFfTaIvQ5pF6UU4ifhK+03lkQmjFpyuLmEse3Dm0YclVBN1h5hAL4AelGHT+XA4ps+sEiS7zvsobHPn0IdB4Xzk2kwYA4/QxCfuV0H6vymaoZ0jo79GOWp6lSl67nTnKO3/yQhwc4cXPTf5EUGjxm2oWaLd+E88K5N03adeHWyNzW9gs5T2smoCikqVk9zK/lieEZMfTL7BpyDybKDAjOi/iLzWNrZZCLFWbhcQQmlzks8YN8S+g3RZuGds3tO5yv4UskfjdljviLczJsU1bmF4T1urA2Ro4jxRXSO4bEDvE3hA6R5BsVEOQH6G8khDmKbfm/wlT7qVq8ktFX5G/2EcDtPtQHZ1K65ZE39ZmzmDd63IVmHl9Fe07GzANO5HILveLtjJppC646G/VkgKG3sfpt0BiWQvgkJzZOob1LmiNpp8Pf0wA5TNwztA3wN2atTTUN4DCne8BENbvfZhppOjBzVPOKajAtaLhkonlj8c4cJAnN7ZisMfD9VeYUOtIS+asqweMSf4yhYlp31NGeu9dWuUZ+dE09gf25iYGjEeAQf6hXLBEmAjcOXtFqoLx1Edxg6SPr9W46oE3N2UvC0LZbWvawD0Tnd6IHFwdzAxGEzZ17jUNxmfqaF2TTiYGbUC3Ix4uod5k55VUzW/glIIiM4Vv3ltCpuWGXIXj7I+ga2dCjsUEQ9QQAw6mNHirbOeUbsbHGei2Y29IoVy0fXk4cykxmvU6qJEB4p9UnqRF7viji/vzwG3ZHwIaV3MkFrQqBQgESbnog2jrpeb/P6n2M7KFVgufBG9ZN2tDzeD9DrfkutkneXZe0HYGqX2Gp4QuxpoWFXax45DMvMfze/oPAUrG9+R+GxSGrLwvx6Kc2wRjriD8HfywCrIiwDNwxReUBdG1+FMVCVch53Y/aM76X5wWYquVY7/2ILpdtVhTkJWq0yyqi3CLH96QjRVzp6oFPXNwIsM0xvyjYtMN5ol270eXDMdZRnTDkbQOCsg709KfWG55ZmASORPwpgLF8iMXw4TupsfU20v0ZsP4CiLLv/GHHa38xfntWCkEINqrf3ChWw0HTeYeTOJvXBqlZ9c9DGvuv7YmiPEEvNMALYj1UWwkiP2xsOaV1zc7WDmCwtIUA0XFL7gKNkXMXASRWAX3gfSyvLCA+9MouXwJ5QIt1UHlL4mqddPlr9bUVrYsB9KDrXdkMItDP8GOCYIxse16sg5ct2zkgT5Th9nRfmLacd3mEzQfMfqsFLwdi4cNYyTQr1W7tqGzKY5W2/vvQunEaBNGviR0TKqpuohb+t2BQBCyGSeI+3g3bhnOq+MFL4EkWa/BW1Ovym0p7tE0BWiYoe2Q61tmVSfe7y5IyplwkdQ8xHLRgCqKxG7vr5F4iBTZYFF6kSgyVTvs1XvhlF7cSyWHvAyUJKR4QZ1jPwMQzdaNTDPx1ZpiTXd9pXT7DDppcbYtXwpKfB1dbnkI7OIEUKDYATfTKOIOXB2TjvtL8mAyFWnmXpXbzrdOVgC6nLIJgf8FtHE5rbhRYMns3+2TZPQzvn4dJsuC3fyQXndCqBjNnpz0v4Y5dhM39uR9zYOak8MXtc14uunj06ZfL6CCeuGp2oeENehQ+jmACGrziGA+1Z13dmgdYFNc4jpLQG7EjJgs77L0dGD7BqqelJc9jNXR9+iY+F3ASAhkm6f4F7UVbvQn+acRFPo+bb/Ppo1hfnKCqwCoa16r1OtRVZOSMtKsWRpkRCkXKqwTO5Iy87Um++sALww2L4iv52TWMOdeGPlaHR73eBLqq6JBs1ch07nuoR87GluTO5+kK/q4SMrzKphdRqpTLF8vrGqeV7IbJjM1WcIEw0hdpxaAkMpEOCPDmllDPgTQKIKmCx5BJCAW2S9/T245Bwy33s/kDcg71zPLJoNFODOGnBDr/HUlDACxTLkkKNXzEz+oMYt85vCbAQZ8eFNySUHocAIRvAOSmmfF6dTkLgIVf4BWUI8GeU1PfkBeizRL72eIE0vY9pEqo6OE5uPGk8fOgC1rV+sEzUpewvmWqCSC8O9OE4u56EpHIETnILfgKME6PzeCFcRd7gVDKjYbAOaFC1Vn90nLC4OfOf1y9exHAjYrGSaNB1FX4nEz4ochzP8keaYapIZh3cL8DscKEv8CgKoLFB8CvDG9uKvHss5W0ykGM3ioBpiGBBmSn81c6bQPuJNxIuz/no7+czJhcNMPHpF2EUmj+81Www9YODm7wHpffyI8cf1UxhQ86kK9izjZK6b/8yIloIAAq59pBTC5tGUE6HKEk+JuxmhCbxnO0WQGgBm5U3j5G02Fl3sIMeX7LcIOUtRwCXT5+/XrObKKNgHYSevZEXMiuTFtKVmjm6i9HC5aUyGgQBCdK10OmtqREL0LlA/UOxoT0HXDwWT5ry6k2SoNGwPlUpNYhCb/H/JfA9QEZcCHagYK4LTZVZfuhSAIuD6YeiesgmY1LhpofHmhBFKKGXEtxsbN8ag5sTo+W8w44q2QLlzyx0cIOwXW+dLyYgRQ5om+gZHGtMS4DfH7+gM/Ouwy3XRtnig+4kAeYG4eN7GHOdD11nlXH3SP8DPselP4UnBCbj3G6mxxv7Hx/cAlj0WjbEQeWKk6V/oUuNZT1sOmApnZpWxWtKbM6ia7yaekljDMABWlFDgso+wZnjSQO6GS/tuZq6HA9gXy1oE1xbZNhVZAjdsrJPIpWvd9+dBMdY6lvJjRMEosddpc2R3RTXiCus9xMlWRur/r9Ye9iR9AZjq2Q3Btmg5KsOXh9r/6M8SqqkleX1qOS31TWsy7shvM2wwrb9fAfe+tGdzRIh+fttSBVtYJl0VfP89kM+rk9ddynmWLjOAZQ3GtWufHk52WeV5fwLZbcmP3bj0E/tFEcQOg2w4FGzkaO8HZMSBHk3m4ByQvJjZSAzMl0aWzo+2TtX35O9ema1OJP8hEzDGN6uWHB01ziC6Tcy746kkTLxWcrPPmxmSUjML1yMKOdABvMOs6lPAIbxggfdGr6+pKH+6SVrTwUw4TU2crR5Po8WWP7GzOMHoAN9rFy5+VChzHVAX4/orIjd1m/cO49Vq3WnQ/07CGz9aldxRSz0oJ8otkyVLOXdOEd+kmXnvr+EB5ms0uqi7ylxs39IJbSS";
+
+        // Divide o conteúdo em partes: salt, iv e dados
+        String[] partes = conteudoCriptografado.split(":");
         if (partes.length != 3) {
-            throw new IllegalArgumentException("Formato inválido");
+            throw new IllegalArgumentException("Formato inválido de conteúdo criptografado");
         }
 
         byte[] salt = Base64.getDecoder().decode(partes[0]);
         byte[] iv = Base64.getDecoder().decode(partes[1]);
-        byte[] conteudoCripto = Base64.getDecoder().decode(partes[2]);
+        byte[] dadosCriptografados = Base64.getDecoder().decode(partes[2]);
 
-        SecretKey chave = gerarChaveSecreta(senha.toCharArray(), salt);
+        // Deriva a chave com a senha
+        SecretKeyFactory fabrica = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA256");
+        KeySpec espec = new PBEKeySpec(senha.toCharArray(), salt, 65536, 256);
+        SecretKey chave = new SecretKeySpec(fabrica.generateSecret(espec).getEncoded(), "AES");
 
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        cipher.init(Cipher.DECRYPT_MODE, chave, new IvParameterSpec(iv));
+        // Define o parâmetro GCM
+        GCMParameterSpec gcmSpec = new GCMParameterSpec(TAG_LENGTH_BIT, iv);
 
-        byte[] bytesDescriptografados = cipher.doFinal(conteudoCripto);
-        return new String(bytesDescriptografados, "UTF-8");
+        // Inicializa o Cipher em modo DECRIPTOGRAFIA
+        Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+        cipher.init(Cipher.DECRYPT_MODE, chave, gcmSpec);
+
+        // Descriptografa os dados
+        byte[] jsonBytes = cipher.doFinal(dadosCriptografados);
+        return new String(jsonBytes, "UTF-8");
     }
 }
